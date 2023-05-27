786c9e5ccdf932b092f14a5381c5b85b738ac539
shawn <shawnsmith0024@gmail.com>
2023-05-27 08:36:01 -0400

    .

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4c529b0505f240a4"; }
+/******/ 		__webpack_require__.h = function() { return "cf60b1dbf6356412"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,9 +13,9 @@ const Hero = (props) => (
           My name is Shawn Smith
         </SectionTitle>
         <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+        I'm Shawn Smith, a full stack developer embarking on an exciting career change from warehouse operations to web development. Throughout my years in logistics, I honed my attention to detail, problem-solving abilities, and knack for efficient workflow management. Now, I'm seamlessly applying these transferable skills to create robust and user-friendly web applications. My expertise in optimizing processes and maximizing productivity directly translates into writing efficient code. Moreover, my strong communication and teamwork skills enable me to collaborate effectively with cross-functional teams. With a deep passion for technology and a commitment to continuous learning.
         </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
+        <Button onClick={() => window.location = 'https://dev.to/shawnsmith24'}>Blog</Button>
       </LeftSection>
     </Section>
   </>
