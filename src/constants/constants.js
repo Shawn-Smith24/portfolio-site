d0e09062cf9bcbd2603503c21818f86f59561e96
shawn <shawnsmith0024@gmail.com>
2023-05-27 08:45:51 -0400

    edits

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,22 +1,14 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: 'My Portfolio',
+    description: "Used React to build my portfolio website to easily stage my experience and all my projects",
+    image: '/images/portfolio-site.png',
+    tags: ['React'],
+    source: 'https://github.com/Shawn-Smith24/portfolio-site',
+    demo: '',
     id: 0,
   },
-  {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 1,
-  },
+
 ];
 
 export const TimeLineData = [
@@ -25,3 +17,15 @@ export const TimeLineData = [
   { year: 2022, text: 'Signed up for Flatiron School Software Engineering Bootcamp', },
   { year: 2023, text: 'Graduated from Flatiron School!', },
 ];
+
+
+//Easy add project template
+// {
+// title: '',
+// description: '',
+// image: '',
+// tags: [''],
+// source: '',
+// demo: '',
+// id: 
+// }
