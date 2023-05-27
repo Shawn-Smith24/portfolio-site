8642ff0f29044b3b52968be26d6207b91e96e744
shawn <shawnsmith0024@gmail.com>
2023-05-27 07:55:34 -0400

    edits

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+
+import { SocialIcons } from '../Header/HeaderStyles';
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:shawnsmith0024@outlook.com">
+            shawnsmith0024@outlook.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Adding creativity to technical problems</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/Shawn-Smith24">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/shawn-r-smith/">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
