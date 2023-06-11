c849ca4620e643e7bfa1062a99cd0cf9415b6432
shawn <shawnsmith0024@gmail.com>
2023-06-11 16:41:41 -0400

    added new project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,23 +9,32 @@ export const projects = [
     id: 0,
   },
   {
-    title: 'Rick & Morty App',
-    description: 'An app using Rick & Morty API to show characters, episodes and various locations through the show',
-    image: '/images/Rick&Morty.png',
-    tags: ['Swift, UIKit, SwiftUI'],
-    source: '',
-    demo: '/images/Rick&MortyDemo.mp4',
-    id: 1
-  },
-  {
     title: 'Job Tracker',
     description: 'The Job Tracker App is a full-stack application built to help users keep track of their job applications. It allows users to sign up, search for jobs, and apply for jobs, as well as manage their application status.',
     image: '/images/Job-board.png',
-    tags: ['Python, Flask, React.js, SQLite, Tailwind'],
+    tags: ['Python, Flask, React.js, SQLite, Tailwind, Redux'],
     source: 'https://github.com/Shawn-Smith24/job-tracker/tree/shawn',
     demo: '',
+    id: 1
+  },
+  {
+    title: 'Rick & Morty App',
+    description: 'An app using Rick & Morty API to show characters, episodes and various locations through the show',
+    image: '/images/Rick&Morty.png',
+    tags: ['Swift, UIKit'],
+    source: '',
+    demo: '/images/Rick&MortyDemo.mp4',
     id: 2
   },
+  {
+    title: 'Netflix Clone',
+    description: 'A clone of Netflix using Swift and UIKit',
+    image: '/images/Netflix.png',
+    tags: ['Swift, UIKit'],
+    source: 'https://github.com/Shawn-Smith24/Netflix-Clone',
+    demo: '/images/NetflixDemo.mp4',
+    id: 3 
+    }
   
 
 ];
