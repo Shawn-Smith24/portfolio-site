cd999feca98a853beb84cc1c623e468e10e49b23
shawn-smith24 <shawnsmith0024@gmail.com>
2023-07-01 17:44:54 -0400

    updates

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DiApple, DiFirebase, DiReact, DiSwift, DiZend } from 'react-icons/di';
+import { DiApple, DiBackbone, DiDatabase, DiFirebase, DiReact, DiSwift, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
@@ -8,7 +8,7 @@ const Technologies = () =>  (
     <SectionDivider divider />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
-      I've worked with a range a technologies in the Software Engineering world both Front-End, Back-End & Mobile Development
+      I've worked with a range a technologies in the Software Engineering world both Front-End & Back-End 
     </SectionText>
     <List>
       <ListItem>
@@ -31,8 +31,20 @@ const Technologies = () =>  (
           <ListTitle>Back-End</ListTitle>
           <ListParagraph>
             Experience with <br />
-            Python, Flask, Django, SQLite, MongoDB,
+            C#, Python, Flask, Django,
             Node, Express
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+       <ListItem>
+        <picture>
+          <DiDatabase size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Databases</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            SQL, MongoDB, SQLite
           </ListParagraph>
         </ListContainer>
       </ListItem>
