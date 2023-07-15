96684c7b7ddc2183463a23f69d212ee888e9a5f9
shawn <shawnsmith0024@gmail.com>
2023-07-15 06:31:44 -0400

    nav bar changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ import { DiCssdeck } from 'react-icons/di';
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
-  <Container>
+  <Container style={{position: 'sticky', top:'0', zIndex:'25', backgroundColor:'black',}}  >
     <Div1>
       <Link href="/">
         <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -9,6 +9,10 @@ export const Container = styled.div`
   padding: 1rem;
   padding-top: 2rem;
 
+ 
+  
+ 
+
   @media ${(props) => props.theme.breakpoints.sm} {
     display: grid;
     grid-template-columns: repeat(5, 1fr);
